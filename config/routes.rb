63920bab58b69d4d76a1813015ea999e97d9e675
Dmitry Abramov <alpha.ru@gmail.com>
2018-03-03 23:08:19 +0300

    add category model

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   resources :users
   get 'welcome/index'
 
-  
   resources :articles do
     resources :comments
   end
 
-
+  resources :article_categories do
+    resources :articles
+  end
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
