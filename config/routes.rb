972f3c6fa1e1cde7a9217f92ff38521a03729254
Dmitry Abramov <alpha.ru@gmail.com>
2018-03-03 08:52:17 +0300

    add russian locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :articles do
-    resources :comments
+  get '/:locale' => 'articles#index'
+
+  scope "(/:locale)" do
+    resources :articles do
+      resources :comments
+    end
   end
+
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
