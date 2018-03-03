2c45a48c9564765073aa00a738791e13a9e05286
Dmitry Abramov <alpha.ru@gmail.com>
2018-03-03 14:43:20 +0300

    add filters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,8 @@ class ArticlesController < ApplicationController
   http_basic_authenticate_with name: 'name', password: 'password', except: [:index, :show]
 
   def index
-    @articles = Article.all
+    @q = Article.ransack(params[:q])
+    @articles = @q.result(distinct: true)
   end
 
   def show
